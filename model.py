e84ac800d9e662890e85e48d3002fa43e280236c
marcosc30 <marcos_carmona@brown.edu>
2024-04-27 18:23:31 -0400

    Added a simple CNNLSTM and CNNTransformer models, need to tweak hyperparameters and add individualized parameters for the CNN layers

--- model.py
+++ model.py
@@ -1,9 +1,9 @@
 import torch
 import torch.nn as nn
 
-class WeatherForecaster(nn.Module):
+class WeatherForecasterTCN(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, output_size, kernel_size=3, dropout=0.2):
-        super(WeatherForecaster, self).__init__()
+        super(WeatherForecasterTCN, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.num_layers = num_layers
@@ -48,4 +48,95 @@ class WeatherForecaster(nn.Module):
         # Use the last hidden state from the LSTM
         out = self.fc(h_n[-1])  # (batch_size, output_size)
         
+        return out
+
+
+class WeatherForecasterCNNLSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size, kernel_size=3, dropout=0.2):
+        super(WeatherForecasterCNNLSTM, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.output_size = output_size
+        self.kernel_size = kernel_size
+
+        # CNN layers
+        self.cnn = nn.Sequential(
+            nn.Conv2d(input_size, hidden_size, kernel_size, padding='same'),
+            nn.batch_norm2d(hidden_size),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Conv2d(hidden_size, hidden_size, kernel_size, padding='same'),
+            nn.batch_norm2d(hidden_size),
+            nn.ReLU(),
+            nn.Dropout(dropout)
+        )
+        
+        # LSTM layers
+        self.lstm = nn.LSTM(hidden_size, hidden_size, num_layers, batch_first=True, dropout=dropout)
+        
+        # Fully connected output layer
+        self.fc = nn.Linear(hidden_size, output_size)
+        
+    def forward(self, x):
+        # x: (seq_len, batch_size, grid_data_length, grids_x, grids_y)
+        # grids_x: number of grids in x direction
+        # grids_y: number of grids in y direction
+
+        # Separate all of the grids in the sequence
+        convoluted_grids = []
+        for grid in x:
+            # Pass through CNN layers
+            convoluted_grid = self.cnn(grid)
+            convoluted_grids.append(convoluted_grid)
+        lstm_out = self.lstm(torch.stack(convoluted_grids))
+        out = self.fc(lstm_out)
+
+        return out
+
+class WeatherForecasterCNNTransformer(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size, kernel_size=3, dropout=0.2):
+        super(WeatherForecasterCNNLSTM, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.output_size = output_size
+        self.kernel_size = kernel_size
+
+        # CNN layers
+        self.cnn = nn.Sequential(
+            nn.Conv2d(input_size, hidden_size, kernel_size, padding='same'),
+            nn.batch_norm2d(hidden_size),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Conv2d(hidden_size, hidden_size, kernel_size, padding='same'),
+            nn.batch_norm2d(hidden_size),
+            nn.ReLU(),
+            nn.Dropout(dropout)
+        )
+        
+        # Transformer layer
+        self.transformer = nn.Sequential(
+            nn.TransformerEncoderLayer(d_model=hidden_size, nhead=8),
+            nn.TransformerEncoder(num_layers=num_layers, encoder_layer=self.transformer)
+        )
+
+        
+        # Fully connected output layer
+        self.fc = nn.Linear(hidden_size, output_size)
+        
+    def forward(self, x):
+        # x: (seq_len, batch_size, grid_data_length, grids_x, grids_y)
+        # grids_x: number of grids in x direction
+        # grids_y: number of grids in y direction
+
+        # Separate all of the grids in the sequence
+        convoluted_grids = []
+        for grid in x:
+            # Pass through CNN layers
+            convoluted_grid = self.cnn(grid)
+            convoluted_grids.append(convoluted_grid)
+        transformer_out = self.transformer(torch.stack(convoluted_grids))
+        out = self.fc(transformer_out)
+
         return out
